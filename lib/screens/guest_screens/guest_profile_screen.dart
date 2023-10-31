bf4b63fe64a5c99326234e243772b020f4c6f264
vadymchan <vadymchan@gmail.com>
2023-10-31 17:59:36 +0200

    add placeholder for unregistered user

---
+++ lib/screens/guest_screens/guest_profile_screen.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+// import 'package:straight_whiskey_mobile/screens/role_section_screen.dart'; // for debugging purposes
+
+class GuestProfileScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      children: ListTile.divideTiles(
+        context: context,
+        tiles: [
+          ListTile(
+            leading: Icon(Icons.info),
+            title: Text('Guest Info'),
+            onTap: () {
+              // navigate to Info screen
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.people),
+            title: Text('Explore Barbers'),
+            onTap: () {
+              // navigate to Explore Barbers screen
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.content_cut),
+            title: Text('Explore Haircuts'),
+            onTap: () {
+              // navigate to Explore Haircuts screen
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.login),
+            title: Text('Log In'),
+            onTap: () {
+              // handle log in
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.app_registration),
+            title: Text('Register'),
+            onTap: () {
+              // handle registration
+            },
+          ),
+          // for debugging purposes
+          // ListTile(
+          //   leading: Icon(Icons.change_circle),
+          //   title: Text('Change Role'),
+          //   onTap: () {
+          //     Navigator.push(
+          //       context,
+          //       MaterialPageRoute(
+          //             builder: (context) => RoleSelectionScreen(),
+          //           ),
+          //     );
+          //   },
+          // ),
+        ],
+      ).toList(),
+    );
+  }
+}
+
+
+//--------------------------------------------------
+
