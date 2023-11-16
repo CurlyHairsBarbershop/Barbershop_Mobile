16d13fe868d5b41e137dd0b0869b120aba618118
Kyrylo Volnykov <kvolnykov@gmail.com>
2023-11-16 21:30:50 +0200

    Fix show services

--- lib/pages/explore_pages/services_page.dart
+++ lib/pages/explore_pages/services_page.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:curly_hairs/services/api_service.dart';
 import 'package:curly_hairs/models/service_model.dart';
@@ -8,30 +10,43 @@ class ServicesPage extends StatefulWidget {
 }
 
 class _ServicesPageState extends State<ServicesPage> {
-  List<Service> services = [];
-
-  @override
-  void initState() async {
-    super.initState();
-    services = await ApiService.getAllServices();
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Services'),
       ),
-      body: ListView.builder(
-        itemCount: services.length,
-        itemBuilder: (context, index) {
-          Service service = services[index];
-          return ListTile(
-            leading: Icon(Icons.cut),
-            title: Text(service.name),
-            subtitle: Text(
-                'Price: \$${service.cost.toStringAsFixed(2)}\nDescription: ${service.description}'),
-          );
+      body: FutureBuilder<List<Service>>(
+        future: ApiService.getAllServices(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            // While waiting for the data to load, display a loading indicator.
+            return CircularProgressIndicator();
+          } else if (snapshot.hasError) {
+            // If there's an error, display an error message.
+            return Text('Error: ${snapshot.error}');
+          } else if (!snapshot.hasData || (snapshot.data?.isEmpty ?? true)) {
+            // If no data is available, display a message indicating no barbers found.
+            return Text('No barbers found.');
+          } else {
+            // Data has been loaded successfully. Display the list of barbers.
+            List<Service> services = snapshot.data ?? [];
+            return ListView.builder(
+              itemCount: services.length,
+              itemBuilder: (context, index) {
+                Service service = services[index];
+                return GestureDetector(
+                  
+                  child: ListTile(
+                    leading: Icon(Icons.image), // replace with your image
+                    title: Text('${service.name}'),
+                    subtitle: Text(
+                '\$${service.cost.toStringAsFixed(2)}\nDescription: ${service.description}'),
+                  ),
+                );
+              },
+            );
+          }
         },
       ),
     );
