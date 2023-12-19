a2486ccc986795713d0584f4afbf43e4260388e2
vadymchan <vadymchan@gmail.com>
2023-12-19 16:18:28 +0200

    add favourite button

--- lib/pages/explore_pages/barber_profile_page.dart
+++ lib/pages/explore_pages/barber_profile_page.dart
@@ -1,10 +1,8 @@
-import 'package:curly_hairs/models/reply_model.dart';
+import 'package:flutter/material.dart';
+import 'package:curly_hairs/models/barber_model.dart';
 import 'package:curly_hairs/models/review_model.dart';
 import 'package:curly_hairs/services/api_service.dart';
 import 'package:curly_hairs/services/user_service.dart';
-import 'package:flutter/material.dart';
-import 'package:curly_hairs/models/barber_model.dart';
-import 'dart:typed_data';
 import 'dart:convert';
 
 class BarberProfilePage extends StatefulWidget {
@@ -18,11 +16,13 @@ class BarberProfilePage extends StatefulWidget {
 
 class _BarberProfilePageState extends State<BarberProfilePage> {
   Barber? selectedBarber;
+  bool? isBarberFavorite;
 
   @override
   void initState() {
     super.initState();
     _fetchBarbers();
+    _checkIfFavorite();
   }
 
   Future<void> _fetchBarbers() async {
@@ -34,76 +34,19 @@ class _BarberProfilePageState extends State<BarberProfilePage> {
     setState(() {}); // Update the UI after finding the barber
   }
 
-  String reviewTitle = ''; // Declare reviewTitle variable
-
-  Future<void> _showReviewDialog(BuildContext context) async {
-    return showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        String reviewContent = '';
-
-        return AlertDialog(
-          title: Text('Write a Review'),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextFormField(
-                onChanged: (value) {
-                  reviewTitle = value; // Assign value to reviewTitle
-                },
-                decoration: InputDecoration(
-                  hintText: 'Review Title',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              SizedBox(height: 10), // Add some space between title and content
-              TextFormField(
-                onChanged: (value) {
-                  reviewContent = value;
-                },
-                maxLines: 3,
-                decoration: InputDecoration(
-                  hintText: 'Type your review here...',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-            ],
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: Text('Submit'),
-              onPressed: () async {
-                print('Title: $reviewTitle');
-                print('Review: $reviewContent');
-                // Call the API service method to post a review here
-                await ApiService.postReview({
-                  'title': reviewTitle,
-                  'content': reviewContent,
-                  'barberEmail': widget.barber.email,
-                  'rating': 0,
-                  // Add other necessary fields for posting a review
-                });
-
-                // You might want to update the UI after posting the review
-                // For example, fetching updated barber data
-
-                Navigator.of(context).pop(); // To close the dialog
-              },
-            ),
-          ],
-        );
-      },
-    );
+  Future<void> _checkIfFavorite() async {
+    isBarberFavorite = await ApiService.getIsBarberFavorite(widget.barber.id);
+    setState(() {});
   }
 
-  Future<bool?> _fetchFavoriteBarbers() async {
-    String? token = await UserService.getToken();
-
-    if (token != null) {
-      return ApiService.getIsBarberFavorite(widget.barber.id);
+  Future<void> _toggleFavoriteStatus() async {
+    if (isBarberFavorite!) {
+      await ApiService.deleteBarberFavorite(widget.barber.id);
     } else {
-      return null;
+      await ApiService.addBarberFavorite(widget.barber.id);
     }
+    // Update the favorite status after toggling
+    _checkIfFavorite();
   }
 
   @override
@@ -111,198 +54,69 @@ class _BarberProfilePageState extends State<BarberProfilePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('${selectedBarber?.name ?? widget.barber.name}\'s Profile'),
-        // If selectedBarber is null, use the widget.barber.name instead
+        actions: [
+          IconButton(
+            icon: isBarberFavorite == true
+                ? Icon(Icons.favorite, color: Colors.red)
+                : Icon(Icons.favorite_border),
+            onPressed: _toggleFavoriteStatus,
+          ),
+        ],
       ),
       body: selectedBarber == null
           ? Center(child: CircularProgressIndicator())
-          : FutureBuilder<bool?>(
-              future: _fetchFavoriteBarbers(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return Center(child: CircularProgressIndicator());
-                } else if (snapshot.hasError) {
-                  return Center(child: Text('Error: ${snapshot.error}'));
-                } else {
-                  bool? isBarberFavorite = snapshot.data;
-                  return ListView(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.only(top: 8.0),
-                        child: Center(
-                          child: ClipOval(
-                            child: Image.memory(
-                              base64.decode(widget.barber.image ?? ''),
-                              width: 120.0, // Adjust the size as needed
-                              height: 120.0, // Adjust the size as needed
-                              fit: BoxFit.cover,
-                            ),
-                          ),
-                        ),
-                      ),
-                      ListTile(
-                        title: Text('Name'),
-                        subtitle: Text(selectedBarber!.name +
-                            ' ' +
-                            selectedBarber!.lastName),
-                      ),
-                      ListTile(
-                        title: Text('Email'),
-                        subtitle: Text(selectedBarber!.email),
-                      ),
-                      ListTile(
-                        title: Text('Phone Number'),
-                        subtitle: Text(selectedBarber!.phoneNumber),
-                      ),
-                      Divider(),
-                      Padding(
-                        padding: EdgeInsets.all(8.0),
-                        child: Text('Reviews',
-                            style: Theme.of(context).textTheme.headline6),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: ElevatedButton(
-                          onPressed: () {
-                            _showReviewDialog(context);
-                          },
-                          child: Text('Write a Review'),
-                        ),
-                      ),
-                      Visibility(
-                          visible: isBarberFavorite != null,
-                          child: Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: ElevatedButton(
-                              onPressed: () async {
-                                // Toggle the favorite status based on current state
-                                if (isBarberFavorite!) {
-                                  await ApiService.deleteBarberFavorite(
-                                      widget.barber.id);
-                                } else {
-                                  await ApiService.addBarberFavorite(
-                                      widget.barber.id);
-                                }
-                                // After the operation, fetch the new favorite status and update the UI
-                                setState(() {
-                                  isBarberFavorite = !isBarberFavorite!;
-                                });
-                              },
-                              style: ElevatedButton.styleFrom(
-                                primary: isBarberFavorite ?? false
-                                    ? Colors.amber
-                                    : Colors.grey[300],
-                                onPrimary: Colors.black,
-                              ),
-                              child: isBarberFavorite ?? false
-                                  ? Text('Remove Favorite')
-                                  : Text('Make Favorite'),
-                            ),
-                          )),
-                      for (var review in selectedBarber!.reviews)
-                        ReviewWidget(review: review, barber: selectedBarber!),
-                      SizedBox(height: 20),
-                    ],
-                  );
-                }
-              },
+          : _buildBarberProfilePage(),
+    );
+  }
+
+  Widget _buildBarberProfilePage() {
+    return ListView(
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(top: 8.0),
+          child: Center(
+            child: ClipOval(
+              child: Image.memory(
+                base64.decode(widget.barber.image ?? ''),
+                width: 120.0, // Adjust the size as needed
+                height: 120.0, // Adjust the size as needed
+                fit: BoxFit.cover,
+              ),
             ),
+          ),
+        ),
+        ListTile(
+          title: Text('Name'),
+          subtitle: Text(selectedBarber!.name + ' ' + selectedBarber!.lastName),
+        ),
+        ListTile(
+          title: Text('Email'),
+          subtitle: Text(selectedBarber!.email),
+        ),
+        ListTile(
+          title: Text('Phone Number'),
+          subtitle: Text(selectedBarber!.phoneNumber),
+        ),
+        Divider(),
+        Padding(
+          padding: EdgeInsets.all(8.0),
+          child: Text('Reviews', style: Theme.of(context).textTheme.headline6),
+        ),
+        ...selectedBarber!.reviews
+            .map((review) =>
+                ReviewWidget(review: review, barber: selectedBarber!))
+            .toList(),
+        SizedBox(height: 20),
+      ],
     );
   }
 }
 
 class ReviewWidget extends StatelessWidget {
   final Review review;
-  Barber barber;
-  final double replyIndent = 20.0; // Indentation for each reply level
+  final Barber barber;
 
   ReviewWidget({required this.review, required this.barber});
-
-  Future<void> _showReplyDialog(
-      BuildContext context, int parentReviewId) async {
-    return showDialog(
-      context: context,
-      builder: (BuildContext context) {
-        String replyContent = '';
-
-        return AlertDialog(
-          title: Text('Write a Reply'),
-          content: TextFormField(
-            onChanged: (value) {
-              replyContent = value;
-            },
-            maxLines: 3,
-            decoration: InputDecoration(
-              hintText: 'Type your reply here...',
-              border: OutlineInputBorder(),
-            ),
-          ),
-          actions: <Widget>[
-            TextButton(
-              child: Text('Submit'),
-              onPressed: () async {
-                // Here, you can handle the reply content
-
-                print('Reply to Review $parentReviewId: $replyContent');
-                await ApiService.postReply({
-                  'reviewId': parentReviewId,
-                  'content': replyContent,
-                });
-
-                // Barber selectedBarber = (await ApiService.getAllBarbers())
-                //   .firstWhere((element) => element.email == barber.email);
-                // barber = selectedBarber;
-                Navigator.of(context).pop(); // To close the dialog
-              },
-            ),
-          ],
-        );
-      },
-    );
-  }
-
-  Widget buildReply(Reply reply, int level, BuildContext context) {
-    return Container(
-      margin: EdgeInsets.all(8.0),
-      decoration: BoxDecoration(
-        border: Border.all(color: Colors.grey, width: 1),
-        borderRadius: BorderRadius.circular(8.0),
-      ),
-      child: Padding(
-        padding: EdgeInsets.only(left: replyIndent * level),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Row(
-              children: [
-                Text(
-                  '${reply.publisher.name}:',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.blue,
-                      fontSize: 20),
-                ),
-                Spacer(),
-                TextButton(
-                  onPressed: () {
-                    // Functionality to write a reply
-                    _showReplyDialog(context, reply.id);
-                    // This could be a dialog or navigation to a new page for writing replies
-                  },
-                  child: Text('Reply'),
-                ),
-              ],
-            ),
-            Text(
-              reply.content,
-              style: TextStyle(fontSize: 18.0),
-            ),
-            for (var subReply in reply.replies)
-              buildReply(subReply, level + 1, context),
-          ],
-        ),
-      ),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -316,37 +130,16 @@ class ReviewWidget extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Padding(
-            padding: EdgeInsets.only(left: replyIndent * 0),
-            child: Row(
-              children: [
-                Text(
-                  '${review.publisher.name}:',
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.blue,
-                      fontSize: 20),
-                ),
-                Spacer(),
-                TextButton(
-                  onPressed: () {
-                    // Functionality to write a reply
-                    _showReplyDialog(context, review.id);
-                    // This could be a dialog or navigation to a new page for writing replies
-                  },
-                  child: Text('Reply'),
-                ),
-              ],
-            ),
+          Text(
+            '${review.publisher.name}:',
+            style: TextStyle(
+                fontWeight: FontWeight.bold, color: Colors.blue, fontSize: 20),
           ),
-          Padding(
-            padding: EdgeInsets.only(left: replyIndent * 0),
-            child: Text(
-              review.content,
-              style: TextStyle(fontSize: 18.0),
-            ),
+          Text(
+            review.content,
+            style: TextStyle(fontSize: 18.0),
           ),
-          for (var reply in review.replies) buildReply(reply, 1, context),
+          // Add other elements like replies if needed
         ],
       ),
     );
